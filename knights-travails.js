a6df979ab0947713bb0fa856bacf4eb2bbec646b
cColds <103373668+cColds@users.noreply.github.com>
2023-03-16 13:05:38 -0500

    Add chess class

--- knights-travails.js
+++ knights-travails.js
@@ -1,3 +1,17 @@
-const chessBoard = Array.from({ length: 8 }, () => Array(8).fill(0));
+// TODO ✔️: Create a Chess class
 
-console.log(chessBoard);
+// TODO: Create an object of 8x8 adjacency matrix
+// TODO: Create a method to move knight pieces two forward each direction, and left and right in four directions
+// TODO: Create a method to prevent out of bound moves, or illegal moves for the knight
+// TODO: Create a method to from a (start point) to b (end point), and output each move
+// TODO: Create a method to get the knight's location
+
+class Chess {
+	constructor() {
+		this.currentLocation = [0, 0];
+		this.board = Array.from({ length: 8 }, () => Array(8).fill(0));
+	}
+}
+
+const knight = new Chess();
+console.log(knight);
