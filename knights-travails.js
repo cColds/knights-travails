f406522b4aa64aa87d81088fe8c472f2ddf6266e
cColds <103373668+cColds@users.noreply.github.com>
2023-03-17 03:58:34 -0500

    Add valid move check

--- knights-travails.js
+++ knights-travails.js
@@ -1,17 +1,43 @@
 // TODO ✔️: Create a Chess class
+// TODO ✔️: Create an object of 8x8 adjacency matrix
 
-// TODO: Create an object of 8x8 adjacency matrix
 // TODO: Create a method to move knight pieces two forward each direction, and left and right in four directions
 // TODO: Create a method to prevent out of bound moves, or illegal moves for the knight
 // TODO: Create a method to from a (start point) to b (end point), and output each move
 // TODO: Create a method to get the knight's location
+// TODO: Store the knight's moves in a graph
 
 class Chess {
 	constructor() {
-		this.currentLocation = [0, 0];
+		this.currentPosition = [0, 0];
 		this.board = Array.from({ length: 8 }, () => Array(8).fill(0));
 	}
+
+	isValidMove(destination) {
+		const [destinationX, destinationY] = destination;
+		if (
+			destinationX < 0 ||
+			destinationX > 8 ||
+			destinationY < 0 ||
+			destinationY > 8
+		)
+			return false;
+
+		const [currentX, currentY] = this.currentPosition;
+		const xPositionDifference = Math.abs(currentX - destinationX);
+		const yPositionDifference = Math.abs(currentY - destinationY);
+		console.log("Difference:", xPositionDifference, yPositionDifference);
+
+		if (xPositionDifference === 1 && yPositionDifference === 2) return true;
+		if (yPositionDifference === 1 && xPositionDifference === 2) return true;
+
+		return false;
+	}
+
+	move(start, end) {}
 }
 
 const knight = new Chess();
-console.log(knight);
+console.log(knight.isValidMove([-1, 2]));
+
+// knight.board[knight.currentPosition[0]][knight.currentPosition[0]]
