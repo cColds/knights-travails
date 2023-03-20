401aa2f99b115834ce264f2d8ebb194a134884cd
cColds <103373668+cColds@users.noreply.github.com>
2023-03-20 12:41:32 -0500

    Clean code

--- knights-travails.js
+++ knights-travails.js
@@ -1,32 +1,30 @@
 class Knight {
 	constructor() {
-		this.currentPosition = [0, 0];
 		this.board = Array.from({ length: 8 }, () => Array(8).fill(0));
-		this.board[0][0] = [0, 0]; // Mark edge of default current position
 	}
 
 	isValidMove = (move) => move >= 0 && move <= 7;
 
-	isDestination(move, destination) {
-		return JSON.stringify(move) === JSON.stringify(destination);
+	isPositionMatching(positionA, positionB) {
+		return JSON.stringify(positionA) === JSON.stringify(positionB);
 	}
 
-	possibleMoves() {
-		const [currentX, currentY] = this.currentPosition;
+	getPossibleMoves() {
+		const [x, y] = this.currentPosition;
 
 		const possibleMoves = [
 			// top left
-			[currentX + 1, currentY - 2],
-			[currentX + 2, currentY - 1],
+			[x + 1, y - 2],
+			[x + 2, y - 1],
 			// top right
-			[currentX + 2, currentY + 1],
-			[currentX + 1, currentY + 2],
+			[x + 2, y + 1],
+			[x + 1, y + 2],
 			// bottom left
-			[currentX - 1, currentY - 2],
-			[currentX - 2, currentY - 1],
+			[x - 1, y - 2],
+			[x - 2, y - 1],
 			// bottom right
-			[currentX - 2, currentY + 1],
-			[currentX - 1, currentY + 2],
+			[x - 2, y + 1],
+			[x - 1, y + 2],
 		];
 
 		return possibleMoves.filter(
@@ -37,22 +35,25 @@ class Knight {
 		);
 	}
 
-	logPath(start, [destinationX, destinationY]) {
-		const backtrackMoves = [];
-		let [currentX, currentY] = this.board[destinationX][destinationY];
-		while (JSON.stringify([currentX, currentY]) !== JSON.stringify(start)) {
+	logPath(start, [endX, endY]) {
+		const backtrackMoves = [[endX, endY]];
+		let [currentX, currentY] = this.board[endX][endY];
+		while (true) {
+			if (this.isPositionMatching(start, [currentX, currentY])) {
+				backtrackMoves.push(start);
+				const isMoveSingular = backtrackMoves.length - 1 === 1;
+
+				console.log(
+					`You made it in ${backtrackMoves.length - 1} ${
+						isMoveSingular ? "move" : "moves"
+					}! Here's your path:`
+				);
+				break;
+			}
+
 			backtrackMoves.push([currentX, currentY]);
 			[currentX, currentY] = this.board[currentX][currentY];
 		}
-
-		backtrackMoves.push(start);
-		backtrackMoves.unshift([destinationX, destinationY]);
-
-		console.log(
-			`You made it in ${
-				backtrackMoves.length - 1
-			} moves! Here's your path:`
-		);
 
 		let i = backtrackMoves.length - 1;
 		while (i >= 0) {
@@ -61,22 +62,28 @@ class Knight {
 		}
 	}
 
-	move(start, destination = this.currentPosition) {
-		const queue = [this.currentPosition];
+	move(start = [0, 0], end = [0, 0]) {
+		const queue = [start];
+		this.currentPosition = start;
+		this.board[start[0]][start[1]] = start;
 
-		while (!this.isDestination(queue[0], destination)) {
+		while (!this.isPositionMatching(queue[0], end)) {
 			queue.shift();
 
-			this.possibleMoves().forEach(([x, y]) => {
+			this.getPossibleMoves().forEach(([x, y]) => {
 				queue.push([x, y]);
 				this.board[x][y] = this.currentPosition;
 			});
 
 			this.currentPosition = queue[0];
 		}
-		this.logPath(start, destination);
+		this.logPath(start, end);
 	}
 }
 
 const knight = new Knight();
-knight.move([0, 0], [7, 7]);
+knight.move([0, 0], [1, 2]);
+
+// TODO: Error handling
+// TODO: Clean code
+// TODO (optional): add UI
